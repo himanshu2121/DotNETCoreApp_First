ec5433b3150186a94c41ec1ba4bdb19a6c47b79a
Himanshu Verma <himanshuquantum@gmail.com>
2023-03-30 13:34:17 +0530

    Project : DotNETCoreApp Comments : Modifying Controller "UsersController" and making it asynchronous

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
+using System.Collections.Immutable;
 using System.Net;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -18,22 +20,44 @@ namespace API.Controllers
 
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = _context.Users.ToList();
+            var users = await _context.Users.ToListAsync();
             return users;
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id)
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
-            var user = _context.Users.SingleOrDefault(u=>u.Id ==id);
+            var user = await _context.Users.SingleOrDefaultAsync(u=>u.Id==id);
             if(user == null){
                 
             }
             return user;
 
         }
+
+
+        //Concept of Asynchronous request 
+        
+        /*
+
+        Real world example - waiters have taken one order from one customer , one order is preparing , 
+        waiter does not wait, he will take another orders as well.
+
+        If we make this asynchronous, sure, we make our requests to the database, but then that gets passed
+        on to a different thread known as a delegate.
+        And in the meantime, that thread can get on the business of handling other requests, maybe to get
+        to the database and get other queries or whatever it needs to do because we have requests coming in
+        to the web server.
+
+        But it isn't blocked whilst it's waiting for the database to return.
+        Now, once the database has fulfilled its part of the request, then it will notify the original thread
+        that this request came in on.
+
+        And then that request can go and pick up the response from the database server and return it to the
+        requesting client.
+        */
         
 
     }
